ff1578b87130865ca5c19bf580dc44af0dbd174d
akana <mao.akana@gmail.com>
2019-09-12 19:07:40 +0700

    update home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,6 +41,7 @@ export default function HomeScreen() {
 
           <Text style={styles.getStartedText}>
             Change this text and your app will automatically reload.
+            Change this text and your app will automatically reload.
           </Text>
         </View>
 
